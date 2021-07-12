09c4607a6ae68585f5b12b702ef72013d1d5ed16
Adrian <adrian.asensio.bdn@gmail.com>
2021-07-12 07:08:09 +0200

    Login is not displayed on choosen env

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,45 @@
+import {useEffect, useRef} from "react";
+import {Auth} from "aws-amplify";
+import {useRouter} from "next/router";
 
-function Login () {
-  return <div>Hi im login</div>
+function Login ({env}) {
+
+  const router = useRouter();
+  const userRef = useRef();
+  const passwordRef = useRef();
+
+  useEffect(() => {
+    if (env==="ENV") {
+      router.push("/");
+    }
+  }, [])
+
+  async function signIn() {
+    return await Auth.signIn(userRef.current.value, passwordRef.current.value);
+  }
+
+  function onClick() {
+    signIn().then(() => {
+      router.push("/");
+    }).catch(error => console.log(error));
+  }
+
+  return (
+    <div>
+      <input ref={userRef}/>
+      <input ref={passwordRef}/>
+      <button onClick={onClick}>Click</button>
+    </div>
+  )
 }
 
 export default Login
+
+export async function getStaticProps() {
+
+  return {
+    props: {
+      env: process.env.ENV
+    }
+  }
+}
